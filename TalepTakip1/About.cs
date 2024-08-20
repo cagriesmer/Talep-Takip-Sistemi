d9d18f69b3860978645dc509515311d0c1d47442
Cagri Esmer <cagriesmer7@gmail.com>
2024-08-20 16:12:45 +0300

    update

--- TalepTakip1/About.cs
+++ TalepTakip1/About.cs
@@ -29,7 +29,7 @@ namespace TalepTakip1
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            System.Diagnostics.Process.Start("https://github.com/cagriesmer/TalepTakip1");
+            System.Diagnostics.Process.Start("https://github.com/cagriesmer/TalepTakip");
         }
 
         private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
